6dc35e6cf16a03a7cd5e1c3250bbe1c951fcf873
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-19 05:30:15 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,10 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, event|
     puts "#{season.to_s.capitalize!}:"
     event.each do |day, supply|
-      puts "#{day.to_s.capitalize}: #{supply.join(", ").capitalize!}"
+      supply.map do |word|
+        word.capitalize!
+      end
+      puts "#{day.to_s.capitalize}: #{supply.join(", ")}"
     end
   end
 end
