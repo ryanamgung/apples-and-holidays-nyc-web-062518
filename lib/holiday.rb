18ed1c55b75d95b75b0d75b00f62598ebaf02cab
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-19 05:23:42 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,9 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
       event.each do |day, supply|
         puts "#{day.to_s.capitalize}: #{supply.join(", ").capitalize}"
       end
-    
   end
-
 end
 
 def all_holidays_with_bbq(holiday_hash)
