ed4b80aa0223084ebbc8adc4564501401554000b
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-19 05:44:53 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,30 +18,37 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1] 
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter].map do |key, value|
+    holiday_hash[:winter][key] << "Balloons"
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  holiday_hash[:spring][:memorial_day] << supply
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash[season] = {holiday_name => supply_array}
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  winter_supplies = []
+  winter_supplies << holiday_hash[:winter].values
+  winter_supplies.flatten
 
 end
 
@@ -53,13 +60,35 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  
+  holiday_hash.each do |season, event|
+    puts "#{season.to_s.capitalize!}:"
+    event.each do |day, supply|
+      array = day.to_s.split("_")
+      holiday = []
+      array.each do |element|
+        holiday << element.capitalize!
+      end
+      output = holiday.join(" ")
+      puts "  #{output}: #{supply.join(", ")}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  array = []
+  holiday_hash.each do |season, day|
+    day.each do |holiday, supplies|
+      supplies.each do |element|
+        if element == "BBQ"
+          array << holiday
+        end
+      end
+    end
+  end
+  array
 end
 
 
