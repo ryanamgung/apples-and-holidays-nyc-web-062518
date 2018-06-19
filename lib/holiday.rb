aa81e6d150b9c1375b6ba8450e9245f22acef381
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-19 05:40:45 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,10 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  array = []
+  holiday_hash.each do |season, day|
+    array << day
+  end
 end
 
 
